0deba3bce282b25ad3dd178d0806e690cd600d01
Armaan rawat <armaanrawat@Armaans-MacBook-Air.local>
2025-07-02 14:06:51 +0530

    fixed bugs

--- src/components/Frameworks.jsx
+++ src/components/Frameworks.jsx
@@ -18,24 +18,17 @@ export function Frameworks() {
     "tailwindcss",
     "vitejs",
     "wordpress",
-    "skill-icons--postgresql-light",
-    "skill-icons--typescript",
-    "skill-icons--python-dark",
-    "stripe",
-    "skill-icons--postman",
-    "skill-icons--mysql-dark",
-    "clerk",
   ];
   return (
     <div className="relative flex h-[15rem] w-full flex-col items-center justify-center">
       <OrbitingCircles iconSize={40}>
         {skills.map((skill, index) => (
-          <Icon key={index} src={`/assets/logos/${skill}.svg`} />
+          <Icon key={index} src={`assets/logos/${skill}.svg`} />
         ))}
       </OrbitingCircles>
       <OrbitingCircles iconSize={25} radius={100} reverse speed={2}>
         {skills.reverse().map((skill, index) => (
-          <Icon key={index} src={`/assets/logos/${skill}.svg`} />
+          <Icon key={index} src={`assets/logos/${skill}.svg`} />
         ))}
       </OrbitingCircles>
     </div>
